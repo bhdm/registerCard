d8fa0d19f3d5796d4a5a390591a1ff42874a8f34
Maxim <bhd.m@ya.ru>
2014-03-23 22:07:16 +0400

    create auth controller & test phone controller

--- src/Crm/MainBundle/Controller/AuthController.php
+++ src/Crm/MainBundle/Controller/AuthController.php
@@ -5,7 +5,13 @@ namespace Crm\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Crm\MainBundle\Entity\Page;
+use Crm\MainBundle\Entity\User;
+use Crm\MainBundle\Entity\Driver;
+use Crm\MainBundle\Entity\Company;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * http://habrahabr.ru/post/128159/
@@ -21,4 +27,47 @@ class AuthController extends Controller
     {
         return array();
     }
+
+    /**
+     * @Route("/auth/authPartyOne", name="auth_party_1" , options={"expose"=true})
+     */
+    public function authPartyOneAction(Request $request){
+        if ($request->getMethod() == 'POST'){
+            $lastName = $request->request->get('lastName');
+            $firstName = $request->request->get('firstName');
+            $phone = $request->request->get('phone');
+
+            $testCode = rand(123456 , 999999);
+
+            $session = new Session();
+            $session->start();
+            $session['user'] = array(
+                'lastName'  => $lastName,
+                'firstName' => $firstName,
+                'phone'     => $phone,
+                'testCode'  => $testCode,
+            );
+            return new Response('success');
+        }else{
+            return new Response('error');
+        }
+    }
+
+    /**
+     * @Route("/auth/testPhone", name="test_phone" , options={"expose"=true})
+     */
+    public function testPhoneAction(Request $request){
+        if ($request->getMethod() == 'POST'){
+            $testCode = $request->request->get('testCode');
+            $session = new Session();
+            $session->start();
+            if ($session['user']['testCode'] ==  $testCode){
+                return new Response('success');
+            }else{
+                return new Response('fail');
+            }
+        }
+    }
+
+
 }
