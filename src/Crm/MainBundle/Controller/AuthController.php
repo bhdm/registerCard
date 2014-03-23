76987eed4b5ebd347712a242d2368f96a72a0663
Maxim <bhd.m@ya.ru>
2014-03-23 22:27:06 +0400

    update uuth

--- src/Crm/MainBundle/Controller/AuthController.php
+++ src/Crm/MainBundle/Controller/AuthController.php
@@ -5,13 +5,14 @@ namespace Crm\MainBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\BrowserKit\Response;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Crm\MainBundle\Entity\Page;
 use Crm\MainBundle\Entity\User;
 use Crm\MainBundle\Entity\Driver;
 use Crm\MainBundle\Entity\Company;
-use Symfony\Component\HttpFoundation\Session\Session;
+
 
 /**
  * http://habrahabr.ru/post/128159/
@@ -40,14 +41,13 @@ class AuthController extends Controller
             $testCode = rand(123456 , 999999);
 
             $session = new Session();
-            $session->start();
-            $session['user'] = array(
+            $session->set('user', array(
                 'lastName'  => $lastName,
                 'firstName' => $firstName,
                 'phone'     => $phone,
                 'testCode'  => $testCode,
-            );
-            return new Response('success');
+            ));
+            return $this->render("CrmMainBundle:Form:phone_code.html.twig", array('phone' => $phone));
         }else{
             return new Response('error');
         }
@@ -60,8 +60,7 @@ class AuthController extends Controller
         if ($request->getMethod() == 'POST'){
             $testCode = $request->request->get('testCode');
             $session = new Session();
-            $session->start();
-            if ($session['user']['testCode'] ==  $testCode){
+            if ($session->get('user')['testCode'] ==  $testCode){
                 return new Response('success');
             }else{
                 return new Response('fail');
