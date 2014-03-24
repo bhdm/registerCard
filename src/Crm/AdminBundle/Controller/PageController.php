9629cc9ba530e3951d004b6a9c1f749cdeac3e0a
Maxim <bhd.m@ya.ru>
2014-03-24 23:02:21 +0400

    admin panel

--- src/Crm/AdminBundle/Controller/PageController.php
+++ src/Crm/AdminBundle/Controller/PageController.php
@@ -5,6 +5,7 @@ namespace Crm\AdminBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Crm\MainBundle\Entity\Page;
 
 class PageController extends Controller
@@ -17,5 +18,25 @@ class PageController extends Controller
     {
         $pages = $this->getDoctrine()->getRepository('CrmMainBundle:Page')->findAll();
         return array('pages' => $pages);
+    }
+
+    /**
+     * @Route("/admin/page-edit", name="page_edit")
+     * @Template("CrmAdminBundle:Page:edit.html.twig")
+     */
+    public function editAction()
+    {
+
+//        return array('pages' => $pages);
+    }
+
+    /**
+     * @Route("/admin/page-add", name="page_add")
+     * @Template("CrmAdminBundle:Page:edit.html.twig")
+     */
+    public function addAction()
+    {
+
+//        return array('pages' => $pages);
     }
 }
