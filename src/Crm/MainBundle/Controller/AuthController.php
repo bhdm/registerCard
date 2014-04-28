f95a2775d9f4f9539aea5ea470c0e299a35228b7
Maxim <bhd.m@ya.ru>
2014-04-28 22:27:40 +0400

    update order sign

--- src/Crm/MainBundle/Controller/AuthController.php
+++ src/Crm/MainBundle/Controller/AuthController.php
@@ -212,9 +212,17 @@ class AuthController extends Controller
     public function generatePdfAction(Request $request){
         if ($request->query->get('ord')!=null){
             $userId = $request->query->get('ord');
+
+            if ($request->query->get('mail')){
+                $mail = $request->query->get('mail');
+            }else{
+                $mail = null;
+            }
+
             $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->findOneById($userId);
             $mpdfService = $this->container->get('tfox.mpdfport');
-            $html = $this->render('CrmMainBundle:Form:doc.html.twig',array('user' => $user));
+
+            $html = $this->render('CrmMainBundle:Form:doc.html.twig',array('user' => $user,'mail' => $mail));
             $arguments = array(
 //                'constructorArgs' => array('utf-8', 'A4-L', 5 ,5 ,5 ,5,5 ), //Constructor arguments. Numeric array. Don't forget about points 2 and 3 in Warning section!
                 'writeHtmlMode' => null, //$mode argument for WriteHTML method
