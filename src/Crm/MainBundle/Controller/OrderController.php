767253c5fbb70403a771a8293fe973250a44d7af
Maxim <bhd.m@ya.ru>
2014-06-17 01:54:35 +0400

    update order

--- src/Crm/MainBundle/Controller/OrderController.php
+++ src/Crm/MainBundle/Controller/OrderController.php
@@ -16,6 +16,7 @@ use Crm\MainBundle\Form\Type\UserType;
 use Crm\MainBundle\Form\Type\DriverType;
 use Crm\MainBundle\Form\Type\CompanyType;
 use Symfony\Component\Form\FormError;
+use Test\Fixture\Document\Image;
 use Zelenin\smsru;
 
 class OrderController extends Controller{
@@ -33,11 +34,44 @@ class OrderController extends Controller{
      */
     public function uploadPassportAction(Request $request){
         $file = $request->files->get('0');
-
+        $base = $this->imgToBase($file->getPathName());
+        $session = $request->getSession();
+        $session->set('passport', array(
+            'info'=>    $file,
+            'content'=> $base
+            )
+        );
         $response = new Response();
         $response->headers->set('Content-Type','image/jpeg');
-        $response->setContent($this->imgToBase($file->getPathName()));
+        $response->setContent($base);
+
         return $response;
+    }
+
+
+
+
+
+    public function cropimage($img,$x,$y,$width,$height){
+        #@todo Создание ресурса
+        $rect = array(
+            'x' => $x,
+            'y' => $y,
+            'width' => $width,
+            'height' => $height,
+        );
+        $res = imagecrop($img, $rect);
+
+        #@todo преобразование ресурса в файл
+    }
+
+    public function BaseToImg($base){
+        $filePathName = tmpfile();
+        $ifp = fopen($filePathName, "wb");
+        $data = explode(',', $base);
+        fwrite($ifp, base64_decode($data[1]));
+        fclose($ifp);
+        return $filePathName;
     }
 
     public function imgToBase($pathName){
@@ -47,5 +81,7 @@ class OrderController extends Controller{
         $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
         return $base64;
     }
+
+
 }
 
