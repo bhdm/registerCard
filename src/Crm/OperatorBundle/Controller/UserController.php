c1d40b575b2d3c9cd42a78697b2c7e287fc994b8
Maxim <bhd.m@ya.ru>
2014-07-27 17:30:13 +0400

    update operator user

--- src/Crm/OperatorBundle/Controller/UserController.php
+++ src/Crm/OperatorBundle/Controller/UserController.php
@@ -191,17 +191,31 @@ class UserController extends Controller{
                 $fileName = $this->saveFile('work');
                 $user->setCopyWork($fileName);
             }
-            $encoders = array(new XmlEncoder(), new JsonEncoder());
-            $normalizers = array(new GetSetMethodNormalizer());
-            $serializer = new Serializer($normalizers, $encoders);
 
-            $jsonContent = $serializer->serialize($user, 'json');
-            $session->set('user', $jsonContent);
+            $user->setLastNumberCard($data->get('oldNumber'));
 
-            $jsonContent = $serializer->serialize($company, 'json');
-            $session->set('company', $jsonContent);
+            $date = new \DateTime($user->getBirthDate());
+            $user->setBirthDate($date);
 
-            $session->save();
+            $date = new \DateTime($user->getPassportIssuanceDate());
+            $user->setPassportIssuanceDate($date);
+
+            $date = new \DateTime($user->getDriverDocDateStarts());
+            $user->setDriverDocDateStarts($date);
+
+            $date = new \DateTime($user->getDriverDocDateEnds());
+            $user->setDriverDocDateEnds($date);
+
+            $user->setCopyPassport($this->getArrayToImg($user->getCopyPassport()));
+            $user->setCopyDriverPassport($this->getArrayToImg($user->getCopyDriverPassport()));
+            $user->setPhoto($this->getArrayToImg($user->getPhoto()));
+            $user->setCopySignature($this->getArrayToImg($user->getCopySignature()));
+            $user->setCopySnils($this->getArrayToImg($user->getCopySnils()));
+            $user->setCopyWork($this->getArrayToImg($user->getCopyWork()));
+            $user->setCopyPetition($this->getArrayToImg($user->getCopyPetition()));
+
+
+
         }
 
         $country = $this->getDoctrine()->getRepository('CrmMainBundle:Country')->findOneById(3159);
@@ -243,5 +257,213 @@ class UserController extends Controller{
      * @Template()
      */
     public function generatePetitionAction(){}
+
+
+    public function cropimage($img, $rect){
+
+        #Получаем оригинальные размеры картинки
+        if ($rect['width'] == 0 or $rect['height'] == 0){
+            return $img;
+        }
+        $pathName = $this->BaseToImg($img);
+        $image = imagecreatefromjpeg($pathName);
+        $crop = imagecreatetruecolor($rect['width'],$rect['height']);
+        imagecopy ( $crop, $image, 0, 0, $rect['x'], $rect['y'], $rect['width'], $rect['height'] );
+        $pathName = tempnam('/tmp','img-');
+        imagejpeg($crop, $pathName);
+        return $this->imgToBase($pathName);
+    }
+
+    public function blackImage($img){
+        $pathName = $this->BaseToImg($img);
+        $image = imagecreatefromjpeg($pathName);
+        imagefilter($image, IMG_FILTER_GRAYSCALE );
+        $pathName = tempnam('/tmp','img-');
+        imagejpeg($image, $pathName);
+        return $this->imgToBase($pathName);
+    }
+
+    public function rotateImage($img,$degree = 90){
+        $pathName = $this->BaseToImg($img);
+        $image = imagecreatefromjpeg($pathName);
+        $rotate = imagerotate($image, $degree, 0);
+        $pathName = tempnam('/tmp','img-');
+        imagejpeg($rotate, $pathName);
+//        imagejpeg($rotate, $pathName);
+        return $this->imgToBase($pathName);
+    }
+
+    public function BaseToImg($base){
+        $filePathName  = tempnam('/tmp','img-');
+        $ifp = fopen($filePathName, "wb");
+        $data = explode(',', $base);
+        fwrite($ifp, base64_decode($data[1]));
+        fclose($ifp);
+        return $filePathName;
+    }
+
+    public function imgToBase($pathName, $mimeType = 'image/jpeg'){
+
+        if ($mimeType != 'image/jpeg'){
+            if ($mimeType == 'image/png' ){
+                $image = imagecreatefrompng($pathName);
+                imagejpeg($image, $pathName);
+                imagedestroy($image);
+            }elseif($mimeType == 'image/gif'){
+                $image = imagecreatefromgif($pathName);
+                imagejpeg($image, $pathName);
+                imagedestroy($image);
+            }elseif( strripos($mimeType, 'bmp') !== false ){
+                $image = $this->ImageCreateFromBMP($pathName);
+                imagejpeg($image, $pathName);
+                imagedestroy($image);
+            }
+
+        }
+
+
+        $path= $pathName;
+        $type = pathinfo($path, PATHINFO_EXTENSION);
+        $data = file_get_contents($path);
+        $base64 = 'data:image/' . $type . ';base64,' . base64_encode($data);
+        return $base64;
+    }
+
+    public function saveFile($type){
+        $session = new Session();
+        $file = $session->get($type);
+        $file = $file['content'];
+        $pathName = $this->BaseToImg($file);
+        $image = imagecreatefromjpeg($pathName);
+        $fileName = $this->genRandomString();
+        $pathName = 'upload/docs/'.$fileName;
+        imagejpeg($image, $pathName);
+        return $pathName;
+    }
+
+    public function genRandomString(){
+        $length = 16;
+        $characters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWZYZ";
+
+        $real_string_length = strlen($characters) ;
+        $string="id";
+
+        for ($p = 0; $p < $length; $p++)
+        {
+            $string .= $characters[mt_rand(0, $real_string_length-1)];
+        }
+
+        return strtolower($string);
+    }
+
+    public function getArrayToImg($img){
+        if ($img == null){
+            $array =  array();
+        }else{
+            $path = $img;
+            $size = filesize($img);
+            $fileName = basename($img);
+            $originalName = basename($img);
+            $mimeType = mime_content_type($img);
+            $array =  array(
+                'path' =>$path,
+                'size' =>$size,
+                'fileName' =>$fileName,
+                'originalName' =>$originalName,
+                'mimeType' =>$mimeType,
+            );
+        }
+//        return serialize($array);
+        return $array;
+
+    }
+
+
+    public function ImageCreateFromBMP($filename){
+//Ouverture du fichier en mode binaire
+        if (! $f1 = fopen($filename,"rb")) return FALSE;
+
+//1 : Chargement des ent�tes FICHIER
+        $FILE = unpack("vfile_type/Vfile_size/Vreserved/Vbitmap_offset", fread($f1,14));
+        if ($FILE['file_type'] != 19778) return FALSE;
+
+//2 : Chargement des ent�tes BMP
+        $BMP = unpack('Vheader_size/Vwidth/Vheight/vplanes/vbits_per_pixel'.
+            '/Vcompression/Vsize_bitmap/Vhoriz_resolution'.
+            '/Vvert_resolution/Vcolors_used/Vcolors_important', fread($f1,40));
+        $BMP['colors'] = pow(2,$BMP['bits_per_pixel']);
+        if ($BMP['size_bitmap'] == 0) $BMP['size_bitmap'] = $FILE['file_size'] - $FILE['bitmap_offset'];
+        $BMP['bytes_per_pixel'] = $BMP['bits_per_pixel']/8;
+        $BMP['bytes_per_pixel2'] = ceil($BMP['bytes_per_pixel']);
+        $BMP['decal'] = ($BMP['width']*$BMP['bytes_per_pixel']/4);
+        $BMP['decal'] -= floor($BMP['width']*$BMP['bytes_per_pixel']/4);
+        $BMP['decal'] = 4-(4*$BMP['decal']);
+        if ($BMP['decal'] == 4) $BMP['decal'] = 0;
+
+//3 : Chargement des couleurs de la palette
+        $PALETTE = array();
+        if ($BMP['colors'] < 16777216)
+        {
+            $PALETTE = unpack('V'.$BMP['colors'], fread($f1,$BMP['colors']*4));
+        }
+
+//4 : Cr�ation de l'image
+        $IMG = fread($f1,$BMP['size_bitmap']);
+        $VIDE = chr(0);
+
+        $res = imagecreatetruecolor($BMP['width'],$BMP['height']);
+        $P = 0;
+        $Y = $BMP['height']-1;
+        while ($Y >= 0)
+        {
+            $X=0;
+            while ($X < $BMP['width'])
+            {
+                if ($BMP['bits_per_pixel'] == 24)
+                    $COLOR = unpack("V",substr($IMG,$P,3).$VIDE);
+                elseif ($BMP['bits_per_pixel'] == 16)
+                {
+                    $COLOR = unpack("n",substr($IMG,$P,2));
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                elseif ($BMP['bits_per_pixel'] == 8)
+                {
+                    $COLOR = unpack("n",$VIDE.substr($IMG,$P,1));
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                elseif ($BMP['bits_per_pixel'] == 4)
+                {
+                    $COLOR = unpack("n",$VIDE.substr($IMG,floor($P),1));
+                    if (($P*2)%2 == 0) $COLOR[1] = ($COLOR[1] >> 4) ; else $COLOR[1] = ($COLOR[1] & 0x0F);
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                elseif ($BMP['bits_per_pixel'] == 1)
+                {
+                    $COLOR = unpack("n",$VIDE.substr($IMG,floor($P),1));
+                    if     (($P*8)%8 == 0) $COLOR[1] =  $COLOR[1]        >>7;
+                    elseif (($P*8)%8 == 1) $COLOR[1] = ($COLOR[1] & 0x40)>>6;
+                    elseif (($P*8)%8 == 2) $COLOR[1] = ($COLOR[1] & 0x20)>>5;
+                    elseif (($P*8)%8 == 3) $COLOR[1] = ($COLOR[1] & 0x10)>>4;
+                    elseif (($P*8)%8 == 4) $COLOR[1] = ($COLOR[1] & 0x8)>>3;
+                    elseif (($P*8)%8 == 5) $COLOR[1] = ($COLOR[1] & 0x4)>>2;
+                    elseif (($P*8)%8 == 6) $COLOR[1] = ($COLOR[1] & 0x2)>>1;
+                    elseif (($P*8)%8 == 7) $COLOR[1] = ($COLOR[1] & 0x1);
+                    $COLOR[1] = $PALETTE[$COLOR[1]+1];
+                }
+                else
+                    return FALSE;
+                imagesetpixel($res,$X,$Y,$COLOR[1]);
+                $X++;
+                $P += $BMP['bytes_per_pixel'];
+            }
+            $Y--;
+            $P+=$BMP['decal'];
+        }
+
+//Fermeture du fichier
+        fclose($f1);
+
+        return $res;
+    }
 
 }
