0e34c873de579b5a25357fb1e072746ab63d1531
bhdm <bhd.m@ya.ru>
2014-08-06 14:32:50 +0400

    update generation photo & passport

--- src/Crm/MainBundle/Controller/OrderController.php
+++ src/Crm/MainBundle/Controller/OrderController.php
@@ -110,7 +110,7 @@ class OrderController extends Controller{
 
 
         if ($type == 'photo' || $type == 'sign'){
-            $base = $this->blackImage($base);
+            $base = $this->blackImage($base, $type);
         }
 
 
@@ -373,10 +373,23 @@ class OrderController extends Controller{
         return $this->imgToBase($pathName);
     }
 
-    public function blackImage($img){
+    public function blackImage($img, $type = null){
         $pathName = $this->BaseToImg($img);
         $image = imagecreatefromjpeg($pathName);
         imagefilter($image, IMG_FILTER_GRAYSCALE );
+
+        if ($type == 'photo'){
+            $crop = imagecreatetruecolor(394,506);
+            imagecopyresized( $crop, $image, 0, 0,0, 0, 394, 506, imagesx($image), imagesy($image) );
+            $image = $crop;
+        }
+
+        if ($type == 'sign'){
+            $crop = imagecreatetruecolor(591,118);
+            imagecopyresized( $crop, $image, 0, 0,0, 0, 591, 118, imagesx($image), imagesy($image) );
+            $image = $crop;
+        }
+
         $pathName = tempnam('/tmp','img-');
         imagejpeg($image, $pathName);
         return $this->imgToBase($pathName);
