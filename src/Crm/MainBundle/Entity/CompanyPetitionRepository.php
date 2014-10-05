1dbf0b75aae4b5d723a83fbfb6f8d17a05af43d3
Maxim <bhd.m@ya.ru>
2014-10-06 01:37:40 +0400

    sdf

--- src/Crm/MainBundle/Entity/CompanyPetitionRepository.php
+++ src/Crm/MainBundle/Entity/CompanyPetitionRepository.php
@@ -10,4 +10,20 @@ class CompanyPetitionRepository extends EntityRepository
     {
         return $this->findBy(array('enabled'=> 1 ), array('created' => 'DESC'));
     }
+
+    public function PetitionModerator($moderator){
+
+        $qb = $this->createQueryBuilder('b');
+        $qb->select('b')
+            ->leftJoin('b.group', 'g')
+            ->andWhere('g = :groupId')
+            ->andWhere('g.enabled = TRUE')
+            ->andWhere('b.enabled = TRUE')
+            ->andWhere('b.starts < CURRENT_TIMESTAMP()')
+            ->andWhere('b.ends IS NULL OR b.ends > CURRENT_TIMESTAMP()')
+            ->andWhere('b.expires IS NULL OR b.expires > 0')
+            ->groupBy('b');
+        return $qb->getQuery()->getResult();
+    }
+
 }
