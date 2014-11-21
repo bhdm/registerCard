b8552b57428530622db1f922fb77f73c683e5d4d
bhdm <bhd.m@ya.ru>
2014-11-21 12:44:23 +0300

    u

--- src/Crm/OperatorBundle/Controller/ImageController.php
+++ src/Crm/OperatorBundle/Controller/ImageController.php
@@ -30,7 +30,7 @@ class ImageController extends Controller
      * @Route("/passport/{id}", name="operator_image_passport")
      * @Template()
      */
-    public function listAction($id)
+    public function passportAction($id)
     {
         $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($id);
         if ($user){
@@ -45,6 +45,107 @@ class ImageController extends Controller
         $response->headers->set('Content-Length', strlen($pdf));
         return $response;
     }
+
+    /**
+     * @Route("/signature/{id}", name="operator_image_signature")
+     * @Template()
+     */
+    public function petitionAction($id)
+    {
+        $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($id);
+        if ($user){
+            $pdf = $this->imageToPdf($user->getCopySignature()['originalName']);
+        }else{
+            $pdf = '';
+        }
+        $response = new Response();
+        $response->setContent($pdf);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Cache-Control', '');
+        $response->headers->set('Content-Length', strlen($pdf));
+        return $response;
+    }
+
+    /**
+     * @Route("/driver/{id}", name="operator_image_driver")
+     * @Template()
+     */
+    public function driverAction($id)
+    {
+        $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($id);
+        if ($user){
+            $pdf = $this->imageToPdf($user->getCopyDriverPassport()['originalName']);
+        }else{
+            $pdf = '';
+        }
+        $response = new Response();
+        $response->setContent($pdf);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Cache-Control', '');
+        $response->headers->set('Content-Length', strlen($pdf));
+        return $response;
+    }
+
+    /**
+     * @Route("/photo/{id}", name="operator_image_photo")
+     * @Template()
+     */
+    public function photoAction($id)
+    {
+        $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($id);
+        if ($user){
+            $pdf = $this->imageToPdf($user->getPhoto()['originalName']);
+        }else{
+            $pdf = '';
+        }
+        $response = new Response();
+        $response->setContent($pdf);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Cache-Control', '');
+        $response->headers->set('Content-Length', strlen($pdf));
+        return $response;
+    }
+
+    /**
+     * @Route("/snils/{id}", name="operator_image_snils")
+     * @Template()
+     */
+    public function snilsAction($id)
+    {
+        $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($id);
+        if ($user){
+            $pdf = $this->imageToPdf($user->getCopySnils()['originalName']);
+        }else{
+            $pdf = '';
+        }
+        $response = new Response();
+        $response->setContent($pdf);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Cache-Control', '');
+        $response->headers->set('Content-Length', strlen($pdf));
+        return $response;
+    }
+
+    /**
+     * @Route("/work/{id}", name="operator_image_work")
+     * @Template()
+     */
+    public function workAction($id)
+    {
+        $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($id);
+        if ($user){
+            $pdf = $this->imageToPdf($user->getCopyWork()['originalName']);
+        }else{
+            $pdf = '';
+        }
+        $response = new Response();
+        $response->setContent($pdf);
+        $response->headers->set('Content-Type', 'application/pdf');
+        $response->headers->set('Cache-Control', '');
+        $response->headers->set('Content-Length', strlen($pdf));
+        return $response;
+    }
+
 
 
     public function imageToPdf($filename){
