217cf7a2aa97bed0a486c488f3a98186783f69c0
bhdm <bhd.m@ya.ru>
2015-02-27 14:42:04 +0300

    u

--- src/Crm/OperatorBundle/Controller/PetitionController.php
+++ src/Crm/OperatorBundle/Controller/PetitionController.php
@@ -99,7 +99,7 @@ class PetitionController extends Controller
         $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->findOneById($userId);
         $mpdfService = $this->container->get('tfox.mpdfport');
         $arguments = array(
-            'constructorArgs' => array('utf-8', 'A4', 5 ,5 ,5 ,5,5 ), //Constructor arguments. Numeric array. Don't forget about points 2 and 3 in Warning section!
+//            'constructorArgs' => array('utf-8', 'A4', 0 ,0 ,0 ,0, 0 ), //Constructor arguments. Numeric array. Don't forget about points 2 and 3 in Warning section!
             'writeHtmlMode' => null, //$mode argument for WriteHTML method
             'writeHtmlInitialise' => null, //$mode argument for WriteHTML method
             'writeHtmlClose' => null, //$close argument for WriteHTML method
