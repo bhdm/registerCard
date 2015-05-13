530ae843676e7bb8c69fa66adc85213a05b76067
bhd.m@ya.ru <bhd.m@ya.ru>
2015-05-14 00:08:36 +0300

    update

--- src/Panel/OperatorBundle/Controller/XmlController.php
+++ src/Panel/OperatorBundle/Controller/XmlController.php
@@ -3,6 +3,7 @@
 namespace Panel\OperatorBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -14,6 +15,7 @@ use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Crm\MainBundle\Entity\Operator;
 use Crm\MainBundle\Entity\OperatorQuotaLog;
 use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 /**
  * @Security("has_role('ROLE_MODERATOR')")
@@ -27,9 +29,33 @@ class XmlController extends Controller
      */
     public function testXmlAction(Request $request)
     {
-        $file = $request->files->get('file');
-        $file = file_get_contents($file->getPathName());
-        $xml = simplexml_load_string($file);
+        if ($request->getMethod() == 'POST'){
+            $file = $request->files->get('file');
+            $file = file_get_contents($file->getPathName());
+            $xml = simplexml_load_string($file);
+        }else{
+            $xml = null;
+        }
         return array('file' => $xml);
+    }
+
+    /**
+     * @Route("/test/pdf", name="panel_operator_xml_pdf")
+     *
+     */
+    public function testXmlPdfAction(Request $request){
+        $file = $request->request->get('pdf-data');
+        if ($file){
+            $response = new Response(base64_decode($file));
+            $response->headers->set('Content-Type', 'application/pdf');
+            $response->headers->set('Cache-Control', '');
+
+
+//            $response = new BinaryFileResponse(base64_decode($file));
+//            $response->headers->set('Content-Type:','application/pdf');
+//            $response->headers->set('Content-Transfer-Encoding:','base64');
+        }
+        return $response;
+
     }
 }
