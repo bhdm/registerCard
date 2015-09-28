04294182f2341b03d0722933fc262e87d21e3569
Maxim <bhd.m@ya.ru>
2015-09-28 12:50:49 +0300

    add password fields

---
+++ src/Crm/AuthBundle/Form/PasswordCompanyType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Crm\AuthBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PasswordCompanyType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('password', 'repeated', array(
+                'type' => 'password',
+                'invalid_message' => 'Пароли должны совпадать',
+                'options' => array('attr' => array('class' => 'password-field')),
+                'required' => true,
+                'first_options'  => array('label' => 'Пароль'),
+                'second_options' => array('label' => 'Повторите пароль'),
+            ))
+
+            ->add('submit', 'submit', array('label' => 'Изменить'))
+        ;
+    }
+
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Crm\MainBundle\Entity\Client'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'crm_authbundle_password';
+    }
+}
