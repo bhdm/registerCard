5b888364d1b3368092207269d372008d560ea882
bhd.m@ya.ru <bhd.m@ya.ru>
2015-10-14 23:36:24 +0300

    u

---
+++ src/Crm/AuthBundle/Handler/AuthenticationHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Crm\AuthBundle\Handler;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Http\Authentication\AuthenticationFailureHandlerInterface;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class AuthenticationHandler implements AuthenticationFailureHandlerInterface, LogoutSuccessHandlerInterface
+{
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $referer = $request->headers->get('referer');
+
+        return new RedirectResponse($referer);
+    }
+
+    public function onLogoutSuccess(Request $request)
+    {
+        $referer = $request->headers->get('referer');
+
+
+        return new RedirectResponse($referer);
+    }
+}
