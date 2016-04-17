c44585e1fab1fa68a2108f0eb01989e818a67d06
bhdm <bhd.m@ya.ru>
2016-04-18 02:06:39 +0300

    u

--- src/Panel/OperatorBundle/Command/getPostStatusCommand.php
+++ src/Panel/OperatorBundle/Command/getPostStatusCommand.php
@@ -35,7 +35,7 @@ class getPostStatusCommand extends ContainerAwareCommand
         foreach ($orders as $order){
             if ($order['post']){
                 $post = $order['post'];
-                $json = "https://www.pochta.ru/tracking?p_p_id=trackingPortlet_WAR_portalportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=getList&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&barcodeList=$post&postmanAllowed=true";
+                $json = json_decode("https://www.pochta.ru/tracking?p_p_id=trackingPortlet_WAR_portalportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=getList&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&barcodeList=$post&postmanAllowed=true");
 //                $output->writeln();
                 var_dump($json);
 //                if ( $json['list'][0]['trackingItem']['hasBeenGiven'] == true ){
