26d191832fcc82962158b404cf148afc9f053c8c
bhdm <bhd.m@ya.ru>
2016-04-18 02:06:13 +0300

    u

--- src/Panel/OperatorBundle/Command/getPostStatusCommand.php
+++ src/Panel/OperatorBundle/Command/getPostStatusCommand.php
@@ -36,7 +36,8 @@ class getPostStatusCommand extends ContainerAwareCommand
             if ($order['post']){
                 $post = $order['post'];
                 $json = "https://www.pochta.ru/tracking?p_p_id=trackingPortlet_WAR_portalportlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=getList&p_p_cacheability=cacheLevelPage&p_p_col_id=column-1&p_p_col_count=1&barcodeList=$post&postmanAllowed=true";
-                $output->writeln($json['list'][0]['trackingItem']['hasBeenGiven']);
+//                $output->writeln();
+                var_dump($json);
 //                if ( $json['list'][0]['trackingItem']['hasBeenGiven'] == true ){
 //                    $txt .= '<a href="https://im-kard.ru/panel/operator/user/edit/'.$order['id'].'" target="_blank">'.$order['id'].' - '.$order['email']." $order[lastName] $order[firstName]".'</a><br />';
 //                }
