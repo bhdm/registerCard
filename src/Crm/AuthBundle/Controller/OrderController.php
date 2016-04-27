d7f9ee2ea7f0b3b3fb646e7634e992abf8564bc2
Maxim <bhd.m@ya.ru>
2016-04-27 15:49:39 +0300

    u

--- src/Crm/AuthBundle/Controller/OrderController.php
+++ src/Crm/AuthBundle/Controller/OrderController.php
@@ -576,9 +576,6 @@ class OrderController extends Controller
         if ($user->getPassportIssuanceDate() != null){
             $user->setPassportIssuanceDate($user->getPassportIssuanceDate()->format('d.m.Y'));
         }
-        if ($user->getPassportIssuanceDate() != null){
-            $user->setPassportIssuanceDate($user->getPassportIssuanceDate()->format('d.m.Y'));
-        }
         if ($user->getDriverDocDateStarts() != null){
             $user->setDriverDocDateStarts($user->getDriverDocDateStarts()->format('d.m.Y'));
         }
