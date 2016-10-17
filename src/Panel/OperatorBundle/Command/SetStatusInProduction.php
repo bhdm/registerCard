c941216ff9c18c3936b23dfe02467aaedcfae014
bhdm <bhd.m@ya.ru>
2016-10-17 14:26:22 +0300

    u

---
+++ src/Panel/OperatorBundle/Command/SetStatusInProduction.php
@@ -0,0 +1,45 @@
+<?php
+namespace Panel\OperatorBundle\Command;
+use
+    Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand,
+    Symfony\Component\Console\Input\InputArgument,
+    Symfony\Component\Console\Input\InputInterface,
+    Symfony\Component\Console\Input\InputOption,
+    Symfony\Component\Console\Output\OutputInterface,
+    Symfony\Component\Process\Process,
+    Crm\MainBundle\Entity\StatusLog;
+class SetStatusInProduction extends ContainerAwareCommand
+{
+    private $testEmails = array('tulupov.m@gmail.com', 'plisenkova.o@evrika.ru');
+
+    protected function configure()
+    {
+        $this->setName('crm:setstatuslog')
+            ->setDescription('Добавляет дату оплаты в пользователя');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $container = $this->getContainer();
+        $em = $container->get('doctrine')->getManager();
+
+        $users = $em->getRepository('CrmMainBundle:User')->findAll();
+
+        foreach ($users as $user){
+            if ($user->getDateInProductionStat() == null && $user->getStatus() >= 2 && $user->getStatus() != 10){
+                $output->writeln($user->getId().' --- ');
+                $date = $user->getCreated();
+                $statusLog = new StatusLog();
+                $statusLog->setCreated($date);
+                $statusLog->setUpdated($date);
+                $statusLog->setTitle('В&nbsp;производстве');
+                $statusLog->setUser($user);
+                $em->persist($statusLog);
+                $em->flush($statusLog);
+            }
+        }
+        $output->write('Конец');
+    }
+
+}
+
