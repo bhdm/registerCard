647f6ad579bcf696f283ce65f9b89fc513836e0a
bhdm <bhd.m@ya.ru>
2016-10-24 10:37:59 +0300

    u

--- src/Panel/OperatorBundle/Controller/XmlController.php
+++ src/Panel/OperatorBundle/Controller/XmlController.php
@@ -117,12 +117,96 @@ class XmlController extends Controller
         return $response;
     }
 
+
     public function imageToBase64($filePath){
         $filePath = __DIR__.'/../../../../web/'.$filePath;
         $imagedata = file_get_contents($filePath);
         $base64 = base64_encode($imagedata);
         return $base64;
     }
+
+    /**
+     * @Route("/ru-xml-mass", name="panel_operator_ru_xml_mass", options={"expose"=true})
+     * @Template("")
+     */
+    public function ruXmlMassAction(Request $request){
+        $zip = new \ZipArchive();
+        $zip_name = time().".zip";
+        if($zip->open($zip_name, \ZIPARCHIVE::CREATE)!==TRUE)
+        {
+            throw $this->createNotFoundException("* Sorry ZIP creation failed at this time;");
+        }
+
+
+        $data = $request->request->get('user');
+        $em = $this->getDoctrine()->getManager();
+        foreach ($data as $key => $val) {
+            $user = $this->getDoctrine()->getRepository('CrmMainBundle:User')->find($key);
+            $filePath = __DIR__.'/../../../../web/';
+
+            $image = new \Imagick($filePath.$user->getPhoto()['path']);
+            $image->setImageFormat('bmp');
+            $files['photo']['base'] = base64_encode($image->getImageBlob());
+            $files['photo']['title'] = 'Photo';
+            $files['photo']['file'] = $user->getPhoto();
+            $image->destroy();
+
+            if (isset($user->getCopyPassport()['path'])){
+                $image = new \Imagick($filePath.$user->getCopyPassport()['path']);
+                $files['passport']['base'] = base64_encode($image->getImageBlob());
+                $files['passport']['title'] = 'Passport';
+                $image->destroy();
+            }
+            if (isset($user->getCopyPassport2()['path'])) {
+                $image = new \Imagick($filePath . $user->getCopyPassport2()['path']);
+                $files['passport2']['base'] = base64_encode($image->getImageBlob());
+                $files['passport2']['title'] = 'Passport2';
+                $image->destroy();
+            }
+            if (isset($user->getCopyDriverPassport()['path'])){
+                $image = new \Imagick($filePath.$user->getCopyDriverPassport()['path']);
+                $files['driver']['base'] = base64_encode($image->getImageBlob());
+                $files['driver']['title'] = 'DriverPassport';
+                $image->destroy();
+            }
+            if (isset($user->getCopyDriverPassport2()['path'])){
+                $image = new \Imagick($filePath.$user->getCopyDriverPassport2()['path']);
+                $files['driver2']['base'] = base64_encode($image->getImageBlob());
+                $files['driver2']['title'] = 'DriverPassport2';
+                $image->destroy();
+            }
+
+            $file = $user->getCopySignature();
+            $file = WImage::ImageToBlackAndWhite($file);
+            $file = WImage::cropSign($file, 591,118);
+            $image = new \Imagick($file);
+            $image->setImageFormat('bmp');
+            $files['signature']['base'] = base64_encode($image->getImageBlob());
+            $files['signature']['title'] = 'Signature';
+            $image->destroy();
+
+            $zip->addFromString($user->getId.'.xml',$this->renderView("PanelOperatorBundle:Xml:xml_ru.html.twig", array('user' => $user,'files' => $files)));
+        }
+
+
+        $zip->close();
+        if(file_exists($zip_name))
+        {
+            header('Content-type: application/zip');
+            header('Content-Disposition: attachment; filename="'.$zip_name.'"');
+            readfile($zip_name);
+            unlink($zip_name);
+        }
+
+//        $response = new Response();
+//        $response->headers->set('Content-Type', 'text/xml');
+//        $content = $this->renderView("PanelOperatorBundle:Xml:xml_ru.html.twig", array('user' => $user,'files' => $files));
+//        $response->headers->set('Content-Disposition', 'attachment;filename="XMLgeneration.xml');
+//        $response->setContent($content);
+//        return $response;
+    }
+
+
 
 
     /**
