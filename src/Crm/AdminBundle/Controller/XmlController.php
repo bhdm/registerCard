7b0036a8c5223452addfa13dfcf93666c2dd9181
bhdm <bhd.m@ya.ru>
2016-11-24 16:47:17 +0300

    u

--- src/Crm/AdminBundle/Controller/XmlController.php
+++ src/Crm/AdminBundle/Controller/XmlController.php
@@ -257,7 +257,7 @@ class XmlController extends Controller
 //        exit;
         $mpdfService = $this->container->get('tfox.mpdfport');
 //        if (is_file('/var/www/upload/tmp/'.$filename)){
-            $html = '<img src="https://im-kard.ru/upload/test.png" style="max-width: 100%"/>';
+            $html = '<img src="https://im-kard.ru/upload/test.png" style=""/>';
 //        }else{
 //            $html = '<img src="https://im-kard.ru/upload/docs/'.$filename.'" style="max-height: 700px; max-width: 100%"/>';
 //        }
