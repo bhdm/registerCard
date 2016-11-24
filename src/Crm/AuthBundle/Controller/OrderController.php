31a8824760fcd5058c35a4370fe8f292fd4f3237
bhdm <bhd.m@ya.ru>
2016-11-25 01:01:33 +0300

    u

--- src/Crm/AuthBundle/Controller/OrderController.php
+++ src/Crm/AuthBundle/Controller/OrderController.php
@@ -82,6 +82,12 @@ class OrderController extends Controller
             $user->setCopySnils($this->getImgToArray($session->get('snilsFile')));
             $user->setCopyInn($this->getImgToArray($session->get('innFile')));
             $user->setCopySignature($this->getImgToArray($session->get('signFile')));
+            copy($session->get('signFile'),$session->get('signFile').'2');
+            copy($session->get('signFile'),$session->get('signFile').'3');
+            copy($session->get('signFile'),$session->get('signFile').'4');
+            $user->setCopySignature2($this->getImgToArray($session->get('signFile')));
+            $user->setCopySignature3($this->getImgToArray($session->get('signFile')));
+            $user->setCopySignature4($this->getImgToArray($session->get('signFile')));
             $user->setPhoto($this->getImgToArray($session->get('photoFile')));
             $user->setTypeCardFile($this->getImgToArray($session->get('typeCardFile')));
 
