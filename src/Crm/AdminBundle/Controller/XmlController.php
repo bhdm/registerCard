3948e03352c22221921650178328937d2e709e3c
bhdm <bhd.m@ya.ru>
2016-11-24 16:41:05 +0300

    u

--- src/Crm/AdminBundle/Controller/XmlController.php
+++ src/Crm/AdminBundle/Controller/XmlController.php
@@ -241,7 +241,7 @@ class XmlController extends Controller
         $image = new \Imagick();
         $image->setResolution(200,200) ;
         $image->readImage($filePath);
-//        $image->setImageUnits(\Imagick::RESOLUTION_PIXELSPERINCH);
+        $image->setImageUnits(\Imagick::RESOLUTION_PIXELSPERINCH);
         $image->resampleImage(200,200,\Imagick::FILTER_UNDEFINED,1);
         $image->setImageFormat("png");
         $image->setImageVirtualPixelMethod(\Imagick::VIRTUALPIXELMETHOD_TRANSPARENT);
