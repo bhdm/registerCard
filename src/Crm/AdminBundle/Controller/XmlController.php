81588c07fa59b29eef0fbca76e689af48196e538
bhdm <bhd.m@ya.ru>
2016-11-29 15:28:27 +0300

    u

--- src/Crm/AdminBundle/Controller/XmlController.php
+++ src/Crm/AdminBundle/Controller/XmlController.php
@@ -265,10 +265,11 @@ class XmlController extends Controller
         $image->compositeImage($file, \Imagick::COMPOSITE_DEFAULT,0,0);
 
 
-
-        $stamp = new \Imagick($this->get('kernel')->getRootDir() . '/../web/bundles/crmmain/images/stamp/stamp_1.png');
+        $stampR = mt_rand(1,5);
+        $rightR = mt_rand(1,3);
+        $stamp = new \Imagick($this->get('kernel')->getRootDir() . '/../web/bundles/crmmain/images/stamp/stamp_'.$stampR.'.png');
         $stamp->resizeImage($stamp->getImageWidth()*0.85, $stamp->getImageHeight()*0.85, \Imagick::FILTER_LANCZOS,1);
-        $right = new \Imagick($this->get('kernel')->getRootDir() . '/../web/bundles/crmmain/images/right/right_1.png');
+        $right = new \Imagick($this->get('kernel')->getRootDir() . '/../web/bundles/crmmain/images/right/right_'.$rightR.'.png');
         $right->resizeImage($right->getImageWidth(), $right->getImageHeight()*0.85, \Imagick::FILTER_LANCZOS,1);
 
         if ($type !== 'doc'){
