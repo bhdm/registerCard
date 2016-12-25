9ea6d8da43666042903b33933629ff94d20869b4
bhdm <bhd.m@ya.ru>
2016-12-25 23:29:48 +0300

    u

--- src/Crm/AdminBundle/Controller/XmlController.php
+++ src/Crm/AdminBundle/Controller/XmlController.php
@@ -306,7 +306,7 @@ class XmlController extends Controller
         }
         if ( $request->query->get('test') == 3){
             try {
-            $image->blurImage(0.7,1.5);
+            $image->blurImage(1,1.5);
             $image->sharpenImage(3,2);
             } catch(\ImagickException $e) {
                 echo 'Error: ' , $e->getMessage();
