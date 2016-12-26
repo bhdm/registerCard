8cd5a7f2d21ab60a97b9fb758f580eda0b3c3880
bhdm <bhd.m@ya.ru>
2016-12-26 21:22:31 +0300

    u

--- src/Crm/AdminBundle/Controller/XmlController.php
+++ src/Crm/AdminBundle/Controller/XmlController.php
@@ -518,7 +518,7 @@ class XmlController extends Controller
 
 
         try {
-            $image->motionBlurImage(1,1.5,0.1);
+            $image->motionBlurImage(1,2,0.9);
             $image->sharpenImage(3,2);
         } catch(\ImagickException $e) {
             echo 'Error: ' , $e->getMessage();
