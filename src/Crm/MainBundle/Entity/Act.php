d4cc225fc941fddffa30ec09f7d238df419f8afc
bhdm <bhd.m@ya.ru>
2016-12-29 15:16:40 +0300

    u

---
+++ src/Crm/MainBundle/Entity/Act.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Crm\MainBundle\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ *
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Act extends BaseEntity
+{
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="Crm\MainBundle\Entity\User", mappedBy="act")
+     */
+    private $users;
+
+    public function __toString()
+    {
+        return $this->title.'';
+    }
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUsers()
+    {
+        return $this->users;
+    }
+
+    /**
+     * @param mixed $users
+     */
+    public function setUsers($users)
+    {
+        $this->users = $users;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param mixed $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+
+
+}
