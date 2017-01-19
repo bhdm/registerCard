2502ce8b2342537cca8e0d5e130bfdc15b48e953
bhdm <bhd.m@ya.ru>
2017-01-19 12:29:35 +0300

    u

--- src/Panel/OperatorBundle/Controller/PaymentController.php
+++ src/Panel/OperatorBundle/Controller/PaymentController.php
@@ -124,6 +124,12 @@ class PaymentController extends Controller
         );
         $clientsList = $this->getDoctrine()->getRepository('CrmMainBundle:Client')->findAll();
         $companyList = $this->getDoctrine()->getRepository('CrmMainBundle:Company')->findAll();
+        if ($this->get('security.context')->isGranted('ROLE_ADMIN')){
+            $companyList = $this->getDoctrine()->getRepository('CrmMainBundle:operator')->findAll();
+        }else{
+            $companyList = array();
+        }
+
         return ['pagination' => $pagination, 'clientsList' => $clientsList, 'companiesList' => $companyList,'sum' => $sum ];
     }
 
