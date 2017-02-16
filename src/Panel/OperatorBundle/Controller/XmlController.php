35bef06273245ff3534d84c70019da65911e0240
bhdm <bhd.m@ya.ru>
2017-02-16 13:42:43 +0300

    u

--- src/Panel/OperatorBundle/Controller/XmlController.php
+++ src/Panel/OperatorBundle/Controller/XmlController.php
@@ -335,9 +335,17 @@ class XmlController extends Controller
     }
 
     public function getOriginImageName($path){
+        $filePath = __DIR__.'/../../../../web/';
         $pathA = explode('/',$path);
+        $pathB = explode('/',$path);
         $pathA[count($pathA)-1] = str_replace('.jpg', '-or.jpg', $pathA[count($pathA)-1]);
 
-        return implode('/',$pathA);
+        $file =  implode('/',$pathA);
+        if (is_file($filePath . $file)){
+            return $file;
+        }else{
+            $pathB[count($pathB)-1] = 'origin-'.$pathB[count($pathB)-1];
+            return  implode('/',$pathB);
+        }
     }
 }
