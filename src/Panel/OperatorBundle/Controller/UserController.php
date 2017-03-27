4780285eb316dda6b6cd22b31cc2e81209210b7d
bhdm <bhd.m@ya.ru>
2017-03-27 17:10:39 +0300

    u

--- src/Panel/OperatorBundle/Controller/UserController.php
+++ src/Panel/OperatorBundle/Controller/UserController.php
@@ -1933,6 +1933,7 @@ class UserController extends Controller
             $phpExcelObject->setActiveSheetIndex(0)->setCellValue('F' . $i, $adrs);
 
         }
+        $phpExcelObject->setActiveSheetIndex(0)->setCellValue('G' . $i, $user->getDeliveryRecipient());
 
         $phpExcelObject->getActiveSheet()->setTitle('Simple');
         // Set active sheet index to the first sheet, so Excel opens this as the first sheet
