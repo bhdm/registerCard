8df60d181483dc154978ffd3f95b11c611ff80e4
bhdm <bhd.m@ya.ru>
2017-04-07 15:46:28 +0300

    u

--- src/Panel/OperatorBundle/Controller/UserController.php
+++ src/Panel/OperatorBundle/Controller/UserController.php
@@ -2485,7 +2485,7 @@ class UserController extends Controller
 
         $operators = $this->getDoctrine()->getRepository('CrmMainBundle:Operator')->findBy(['highOperator' => $this->getUser()]);
         $companies = $this->getDoctrine()->getRepository('CrmMainBundle:Company')->findHigh($this->getUser());
-        return ['users' => $users, 'operators' => $operators, 'companies' => $companies, 'params' => $request->query->get('params'), 'priceToSuccess' => $priceToSuccess];
+        return ['users' => $users, 'operators' => $operators, 'companies' => $companies, 'params' => $request->query->get('params'), 'usersToSuccess' => $usersToSuccess];
     }
 
     /**
