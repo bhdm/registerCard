e12a2ba8b5cb6770737aaf680e7c2b26a47e7bf5
Maxim <bhd.m@ya.ru>
2017-08-02 17:03:40 +0300

    u

--- src/Crm/MainBundle/Controller/AuthController.php
+++ src/Crm/MainBundle/Controller/AuthController.php
@@ -273,6 +273,8 @@ class AuthController extends Controller
                 $file1->setImageFormat('jpg');
                 $base64_1 = 'data:image/jpg;base64,' . base64_encode($file1->getImageBlob());
                 $html = '<img src="'.$base64_1.'" style="width: 200mm"/><br />';
+                $html .= '<img src="'.$base64_1.'" style="width: 200mm"/><br />';
+                $html .= '<img src="'.$base64_1.'" style="width: 200mm"/><br />';
 //            <br style="box-decoration-break: slice;"/>
                 if (isset($user->getCopyOrder2()['path'])){
                     $file2 = new \Imagick('/var/www/'.$user->getCopyOrder2()['path']);
