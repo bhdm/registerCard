ccbd68b067de2a25bc67a78e4158f8668dabb8d0
Maxim <bhd.m@ya.ru>
2017-08-02 17:03:02 +0300

    u

--- src/Crm/MainBundle/Controller/AuthController.php
+++ src/Crm/MainBundle/Controller/AuthController.php
@@ -271,15 +271,15 @@ class AuthController extends Controller
                 $file1 = new \Imagick('/var/www/'.$user->getCopyOrder()['path']);
                 $file1->setFormat('jpg');
                 $file1->setImageFormat('jpg');
-                $base64_1 = 'data:image/jpeg;base64,' . base64_encode($file1->getImageBlob());
-                $html = '<img src="'.$base64_1.'" style="max-width: 100%"/>';
+                $base64_1 = 'data:image/jpg;base64,' . base64_encode($file1->getImageBlob());
+                $html = '<img src="'.$base64_1.'" style="width: 200mm"/><br />';
 //            <br style="box-decoration-break: slice;"/>
                 if (isset($user->getCopyOrder2()['path'])){
                     $file2 = new \Imagick('/var/www/'.$user->getCopyOrder2()['path']);
 //                    $file2->setFormat('jpg');
 //                    $file2->setImageFormat('jpg');
-                    $base64_2 = 'data:image/jpeg;base64,' . base64_encode($file2->getImageBlob());
-                    $html .= '<img src="'.$base64_2.'" style="max-width: 100%"/>';
+                    $base64_2 = 'data:image/jpg;base64,' . base64_encode($file2->getImageBlob());
+                    $html .= '<img src="'.$base64_2.'" style="width: 200mm"/>';
                 }
 //                echo $html;
 //                exit;
