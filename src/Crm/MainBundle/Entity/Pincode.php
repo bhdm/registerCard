d66b1e8a70ee4339b5fc3dd04848e53bb00d335d
Maxim <bhd.m@ya.ru>
2017-08-15 11:23:22 +0300

    add entity pincode

---
+++ src/Crm/MainBundle/Entity/Pincode.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace Crm\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Security\Core\User\EquatableInterface;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * PinCode
+ *
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class Pincode extends BaseEntity
+{
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    protected $fio;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    protected $code;
+
+    /**
+     * @ORM\Column(type="string", length=250)
+     */
+    protected $email;
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=true)
+     */
+    protected $pin;
+
+    /**
+     * @ORM\Column(type="string", length=250, nullable=true)
+     */
+    protected $puk;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Crm\MainBundle\Entity\Client")
+     */
+    protected $client;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    protected $status;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    protected $dateSend;
+
+    public function __construct()
+    {
+        $this->status = 0;
+        $this->enabled = true;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFio()
+    {
+        return $this->fio;
+    }
+
+    /**
+     * @param mixed $fio
+     */
+    public function setFio($fio)
+    {
+        $this->fio = $fio;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @param mixed $code
+     */
+    public function setCode($code)
+    {
+        $this->code = $code;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPin()
+    {
+        return $this->pin;
+    }
+
+    /**
+     * @param mixed $pin
+     */
+    public function setPin($pin)
+    {
+        $this->pin = $pin;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * @param mixed $client
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateSend()
+    {
+        return $this->dateSend;
+    }
+
+    /**
+     * @param mixed $dateSend
+     */
+    public function setDateSend($dateSend)
+    {
+        $this->dateSend = $dateSend;
+    }
+
+    public function getStatusStringTwig()
+    {
+        switch ($this->status) {
+            case null:
+                $status = '<span class="status" style="border: 1px solid #000; color: #000000">Новая</span>';
+                break;
+            case 0:
+                $status = '<span class="status" style="border: 1px solid #000; color: #000000">Новая</span>';
+                break;
+            case 1:
+                $status = '<span class="status" style="border: 1px solid #cc8140; color: #cc8140">Оплаченная</span>';
+                break;
+            case 2:
+                $status = '<span class="status" style="border: 1px solid #33CC33; color: #33CC33">Ожидает получения кода</span>';
+                break;
+            case 3:
+                $status = '<span class="status" style="border: 1px solid #2135cc; color: #2135cc">Выполнена</span>';
+                break;
+        }
+        return $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPuk()
+    {
+        return $this->puk;
+    }
+
+    /**
+     * @param mixed $puk
+     */
+    public function setPuk($puk)
+    {
+        $this->puk = $puk;
+    }
+
+
+}
---
+++ src/Panel/OperatorBundle/Controller/PincodeController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Panel\OperatorBundle\Controller;
+
+use Crm\AuthBundle\Form\AdminClientAddType;
+use Crm\AuthBundle\Form\AdminClientType;
+use Crm\MainBundle\Entity\Client;
+use Crm\MainBundle\Entity\Company;
+use Crm\MainBundle\Entity\CompanyQuotaLog;
+use Crm\MainBundle\Form\CompanyType;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Core\Encoder\MessageDigestPasswordEncoder;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
+
+/**
+ * @Security("has_role('ROLE_ADMIN')")
+ * @Route("/panel/operator/pincode")
+ */
+class PincodeController extends Controller
+{
+    /**
+     * @Route("/list", name="panel_pincode_list")
+     * @Template("")
+     */
+    public function listAction(Request $request){
+        $codes = $this->getDoctrine()->getRepository('CrmMainBundle:Pincode')->findAll();
+
+        $paginator = $this->get('knp_paginator');
+        $pagination = $paginator->paginate(
+            $codes,
+            $this->get('request')->query->get('page', 1),
+            50
+        );
+
+        return ['pagination' => $pagination];
+    }
+
+
+    /**
+     * @Route("/add-pin/{id}", name="panel_pincode_add_pin")
+     */
+    public function addPinCodeAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $item = $this->getDoctrine()->getRepository('CrmMainBundle:Pincode')->findOneById($id);
+        $code = $item->setPin($request->request->get('pin'));
+
+        return new JsonResponse(['status' => 'Ok']);
+    }
+
+    /**
+     * Отправка сообщения для предостовления кода
+     * @Route("/get-pin/{id}", name="panel_pincode_get_pin")
+     */
+    public function getPinCodeAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $item = $this->getDoctrine()->getRepository('CrmMainBundle:Pincode')->findOneById($id);
+        $code = $item->setPin($request->request->get('pin'));
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Заявка отправлена')
+            ->setFrom('info@im-kard.ru')
+            ->setTo('bhd.m@ya.ru')
+            ->setBody(
+                $this->renderView(
+                    '@PanelOperator/Mail/getCode.html.twig',
+                    array('code' => $code)
+                ), 'text/html'
+            )
+        ;
+        $this->get('mailer')->send($message);
+        $code->setStatus(2);
+        $em->flush($code);
+
+        $referer = $request->headers->get('referer');
+        return $this->redirect($referer);
+    }
+
+    /**
+     * Отправка сообщения для предостовления кода
+     * @Route("/send-pin/{id}", name="panel_pincode_send_pin")
+     */
+    public function sendPinCodeAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $item = $this->getDoctrine()->getRepository('CrmMainBundle:Pincode')->findOneById($id);
+        $code = $item->setPin($request->request->get('pin'));
+
+        $message = \Swift_Message::newInstance()
+            ->setSubject('Заявка отправлена')
+            ->setFrom('info@im-kard.ru')
+            ->setTo($code->getEmail())
+            ->setBody(
+                $this->renderView(
+                    '@PanelOperator/Mail/getCode.html.twig',
+                    array('code' => $code)
+                ), 'text/html'
+            )
+        ;
+        $this->get('mailer')->send($message);
+
+        $code->setStatus(2);
+        $em->flush($code);
+
+        $referer = $request->headers->get('referer');
+        return $this->redirect($referer);
+    }
+
+
+    /**
+     * @Route("/remove/{id}", name="panel_pincode_delete")
+     */
+    public function removeAction(Request $request, $id){
+        $em = $this->getDoctrine()->getManager();
+        $item = $this->getDoctrine()->getRepository('CrmMainBundle:Pincode')->findOneById($id);
+        $item->setEnabled(false);
+        $em->flush();
+        $referer = $request->headers->get('referer');
+        return $this->redirect($referer);
+    }
+
+}
