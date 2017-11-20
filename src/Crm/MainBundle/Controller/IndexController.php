2e3ecc44ef6b5dc3eebbc23e36d0c915ea4a14d3
Maxim <bhd.m@ya.ru>
2017-11-20 16:07:17 +0300

    u

--- src/Crm/MainBundle/Controller/IndexController.php
+++ src/Crm/MainBundle/Controller/IndexController.php
@@ -601,7 +601,8 @@ class IndexController extends Controller
         $code = $request->request->get('code');
         $issuances = $this->getDoctrine()->getRepository('CrmMainBundle:PassportCode')->findBy(['code' => $code]);
         $json = [];
-
+        dump($issuances);
+        exit;
         foreach ($issuances as $val){
             $json[] = [
                 'id' => $val->getId(),
