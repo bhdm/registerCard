22c31c845650c1b2f0a0662d8c115e10026d6781
Maxim <bhd.m@ya.ru>
2017-11-28 14:57:42 +0300

    u

--- src/Crm/MainBundle/Controller/AuthController.php
+++ src/Crm/MainBundle/Controller/AuthController.php
@@ -384,7 +384,7 @@ class AuthController extends Controller
 
         $pdfLink = 'https://im-kard.ru'.$this->generateUrl('generate_pdf_statement', ['id' => $id, 'old' => 1]);
         $page1 = new \Imagick();
-        $page1->setResolution(150,150);
+        $page1->setResolution(200,200);
         $page1->readImage($pdfLink.'[0]');
         $page1->setFormat('jpg');
         $page1->setImageFormat('jpg');
