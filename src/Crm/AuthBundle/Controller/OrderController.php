3479627be558837eeeefeaa3db9d911432ad48a1
Maxim <bhd.m@ya.ru>
2018-01-19 13:33:07 +0300

    u

--- src/Crm/AuthBundle/Controller/OrderController.php
+++ src/Crm/AuthBundle/Controller/OrderController.php
@@ -165,8 +165,8 @@ class OrderController extends Controller
 
             $files2 = $request->files;
             foreach ($files2 as $key => $file){
-                $ex = $file->getExtension();
-                $filename = $user->getId().'-'.$key.'.'.$ex;
+//                $ex = $file->getExtension();
+                $filename = $user->getId().'-'.$key.'.jpg';
                 $rootDir2 = __DIR__.'/../../../../web/upload/origin/';
                 $file->move($rootDir2, $filename);
             }
