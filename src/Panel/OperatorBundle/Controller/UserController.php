02f9c9c709a73cf4f712ba43cdc17ee9a6ab9449
Maxim <bhd.m@ya.ru>
2018-02-06 14:54:58 +0300

    u

--- src/Panel/OperatorBundle/Controller/UserController.php
+++ src/Panel/OperatorBundle/Controller/UserController.php
@@ -1417,7 +1417,7 @@ class UserController extends Controller
             $phone = $user->getPhone(); // Телефон абонента
 
 
-            $text = $request->query->get('txt');
+            $text = base64_decode($request->query->get('txt'));
             $sender_name = 'IM-KARD.RU';
             if ($phone){
                 $result = $this->smsapi_push_msg_nologin($email, $password, $phone, $text, array("sender_name"=>$sender_name));
