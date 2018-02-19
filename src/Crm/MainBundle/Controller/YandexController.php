e5575e6e23cc1eabe95d1798b2a8d4308962f28c
Maxim <bhd.m@ya.ru>
2018-02-19 14:22:38 +0300

    u

--- src/Crm/MainBundle/Controller/YandexController.php
+++ src/Crm/MainBundle/Controller/YandexController.php
@@ -69,6 +69,8 @@ class YandexController extends Controller
               'shopId' => $shopId,
               'invoiceId' => $invoiceId,
           ]));
+
+          return $response;
       }
 
     }
