fc8545cfa035c3771472aa059f46762698c12986
Maxim <bhd.m@ya.ru>
2019-01-21 21:53:18 +0300

    u

---
+++ src/Crm/MainBundle/Twig/AppExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CrmMainBundle\Twig;
+
+use Cocur\Slugify\Slugify;
+
+class AppExtension extends \Twig_Extension
+{
+    public function getFilters()
+    {
+        return array(
+            new \Twig_SimpleFilter('translate', array($this, 'translateFilter')),
+        );
+    }
+
+    public function translateFilter($string)
+    {
+        $slugify = new Slugify(null, ['lowercase' => false]);
+        $slugify->addRule('й','y');
+        $string = ucfirst($slugify->slugify($string));
+
+        return $string;
+    }
+
+    public function getName()
+    {
+        return 'app_extension';
+    }
+}
